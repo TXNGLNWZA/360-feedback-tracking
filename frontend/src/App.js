da48288c3afe1fc52650a226b30f1e6ec7e66b16
Tanchanok Juntongkaew <131621433+TXNGLNWZA@users.noreply.github.com>
2025-06-29 23:43:10 +0700

    Initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserHome from "./pages/UserHome";
 import EvaluationForm from "./pages/EvaluationForm";
 import AdminHome from "./pages/AdminHome";
+import AdminDashboard from "./pages/AdminDashboard";
+import LoginPage from "./pages/Login";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<UserHome />} />
+        <Route path="/" element={<LoginPage />} />                    
+        <Route path="/userhome" element={<UserHome />} />
         <Route path="/evaluate/:id" element={<EvaluationForm />} />
-        <Route path="/admin" element={<AdminHome />} />  {/* <-- เพิ่มบรรทัดนี้ */}
+        <Route path="/admin" element={<AdminHome />} />
+        <Route path="/admin-dashboard/:evaluatorId" element={<AdminDashboard />} />
       </Routes>
     </Router>
   );
