478b089a6c38289dca72d0dec3e2752268b3ad27
Tanchanok Juntongkaew <131621433+TXNGLNWZA@users.noreply.github.com>
2025-07-02 02:46:49 +0700

    Update db.js

--- backend/models/db.js
+++ backend/models/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  // ssl: { rejectUnauthorized: false } // เปิดเฉพาะถ้าใช้ PostgreSQL ฟรีที่ต้องการ ssl เช่น Render Database หรือ ElephantSQL
+  ssl: { rejectUnauthorized: false } // <<-- เพิ่มบรรทัดนี้
 });
 
 module.exports = pool;
